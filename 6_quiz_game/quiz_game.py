607b468842bbe45dc7938f8169251559fed12e56
Justin Landess <justinlandess@Justins-MacBook-Pro.local>
2025-02-10 20:02:45 -0500

    updated quiz_game.py to a working version

--- 6_quiz_game/quiz_game.py
+++ 6_quiz_game/quiz_game.py
@@ -30,24 +30,25 @@ while True:
         print("please enter a, b, c, or d")  
 
 
-correct_answers = {answer_1:"C",answer_2:"B",answer_3:"D"}
-if answer_1 == correct_answers[answer_1]:
+correct_answers = {"Question_1":"C","Question_2":"B","Question_3":"D"}
+if answer_1 == correct_answers["Question_1"]:
     scores += 1
     print("Question 1 is correct!")
 else:
     print("Question 1 is incorrect")
 
-if answer_2 == correct_answers[answer_2]:
+if answer_2 == correct_answers["Question_2"]:
     scores += 1
     print("Question 2 is correct!")
 else:
     print("Question 2 is incorrect")
 
-if answer_3 == correct_answers[answer_3]:
+if answer_3 in correct_answers["Question_3"]:
     scores += 1
+    
     print("Question 3 is correct!")
 else:
     print("Question 3 is incorrect")
-
+    
 print(f"You got {scores}/3 correct") 
 
