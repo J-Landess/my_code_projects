7870d21a9ed9de1b3fa3f8a5c91fc517d9a4e363
J-Landess <jtlandess@icloud.com>
2025-02-17 12:54:11 -0500

    Quiz game update

--- 6_quiz_game/quiz_game_v3.py
+++ 6_quiz_game/quiz_game_v3.py
@@ -3,7 +3,6 @@ from termcolor import colored, cprint
 
 scores = 0
 valid_answers = ["A","B","C","D",]
-#correct_answers = {answer_1:"C",answer_2:"B",answer_3:"D"}
 
 def get_answer_1(scores=0):
     while True:
@@ -72,51 +71,3 @@ def run():
     print(f"{colrtxt}")
 
 run()
-
-
-
-
-
-
-
-
-
-# def compute_score(answer_1, answer_2, answer_3, scores):
-#     print(scores)
-#     correct_answers = {"question_1":["C","c"],"question_2":["B","b"],"question_3":["D","d"]}
-#     if answer_1 in correct_answers["question_1"]:
-#         scores += 1
-#         text = colored("Question 1 is correct!","green",)
-#         print(text)
-#     else:
-#         text = colored("Question 1 is incorrect","red",)
-#         print(text)
-
-#     if answer_2 in correct_answers["question_2"]:
-#         scores += 1
-#         text = colored("Question 2 is correct!","green",)
-#         print(text)
-#     else:
-#         text = colored("Question 2 is incorrect","red",)
-#         print(text)
-
-
-#     if answer_3 in correct_answers["question_3"]:
-#         scores += 1
-#         text = colored("Question 3 is correct!","green",)
-#         print(text)
-#     else:
-#         text = colored("Question 3 is incorrect","red",)
-#         print(text)
-
-
-#     text = colored(f"You got {scores}/3 correct","blue",attrs=["underline","blink"])
-#     print(text) 
-#     return scores
-# correct_answers = {"question_1":["C",],"question_2":["B",],"question_3":["D",]}
-# answer_1 = get_answer_1(scores)
-# answer_2 = get_answer_2(scores)
-# answer_3 = get_answer_3(scores)
-# correct_answers = {"question_1":["C",],"question_2":["B",],"question_3":["D",]}
-# scores = compute_score(answer_1, answer_2, answer_3, scores)
-
