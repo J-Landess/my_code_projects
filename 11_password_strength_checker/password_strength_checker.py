325b4a7feecaa78a11bbf636eead5444fa4b6269
J-Landess <jtlandess@icloud.com>
2025-02-26 21:53:27 -0500

    "first draft of password_strength_checker.py"

---
+++ 11_password_strength_checker/password_strength_checker.py
@@ -0,0 +1,39 @@
+special_characters = ['!','@','#','$','%','^','&','*','(',')',]
+numbers = ['1','2','3','4','5','6','7','8','9','0',]
+
+user_password = input("Please enter a Password: ")
+password_strength = 0
+if len(user_password) > 4:
+    print("Length + 4: Check")
+    password_strength += 1 
+# print(password_strength)
+if len(user_password) > 8:
+    print("Length + 8: Check")
+    password_strength += 1
+# print(password_strength)
+
+if any(i in user_password for i in special_characters):
+    print("Special Characters: Check")  
+    password_strength += 1
+    # print(password_strength)
+if any(i in user_password for i in user_password.upper()):
+    print("Uppercase or number: Check") 
+    password_strength += 1
+    # print(password_strength)
+if any(i in user_password for i in numbers):
+    print("Number: Check")
+    password_strength += 1
+# print(password_strength)
+
+if password_strength == 0:
+    print("must at least contain four characters")
+if password_strength == 1:
+    print("Weak")
+if password_strength == 2:
+    print("Fair")
+if password_strength == 3:
+    print("Good")
+if password_strength == 4:
+    print("Very Good") 
+if password_strength == 5:
+    print("Very Strong")
