d677ac9cd8e3244d65b5bf4a423e78d3f641c3b4
J-Landess <jtlandess@icloud.com>
2025-02-27 19:16:10 -0500

    First draft of this password generator, feeling much more comfortable with these level of problems. Want to understand list comprehensions a little more in depth

---
+++ 12_password_generator/password_generator.py
@@ -0,0 +1,42 @@
+import random
+import string
+numbers = string.digits
+letters = string.ascii_letters
+special_characters = string.punctuation
+u_letters = letters.upper()
+l_letters = letters.lower()
+all_characters = numbers+letters+special_characters
+# enter password length
+length = int(input("How long do you want your password?: "))
+uppercase = input("include uppercase letters [y/n]?: ")
+lowercase = input("include lowercase letters [y/n]?: ")
+special = input("include special characters [y/n]?: ")
+
+if uppercase == "y" and lowercase == "y" and special == "y":
+    for i in range(length):
+        password = [random.choice(all_characters) for character in range(length)]
+    print(password)
+elif uppercase == "n" and lowercase == "n" and special == "n":
+    for i in range(length):
+        password = [random.choice(numbers) for character in range(length)]
+    print(password)
+elif uppercase == "y" and lowercase == "y" and special == "n":
+    for i in range(length):
+        password = [random.choice(u_letters + l_letters + numbers) for character in range(length)]
+    print(password)
+elif uppercase == "y" and lowercase == "n" and special == "n":
+    for i in range(length):
+        password = [random.choice(u_letters + numbers) for character in range(length)]
+    print(password)
+elif uppercase == "n" and lowercase == "n" and special == "y":
+    for i in range(length):
+        password = [random.choice(special_characters + numbers) for character in range(length)]
+    print(password)
+elif uppercase == "n" and lowercase == "y" and special == "n":
+    for i in range(length):
+        password = [random.choice(l_letters + numbers) for character in range(length)]
+    print(password)
+# generate password from user inputs
+# print to screen
+# 
+# 
